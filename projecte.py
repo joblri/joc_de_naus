d7d28049360f6b35de2bbb7786def2d9d47824dd
joblri <joel.blanch@iaballester.cat>
2025-05-08 11:35:42 +0200

    Update projecte.py

--- projecte.py
+++ projecte.py
@@ -1,61 +1,152 @@
-import pygame, sys
-from pygame.locals import *
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        pygame.quit()
 
-AMPLE = 800
-ALT = 600
-TAMANY = (AMPLE,ALT)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-INDIGO = (75,0,130)
-ORANGE = (255,102,0)
-YELLOW = (255,213,0)
-VIOLET = (127,0,255)
-GREY = (150,150,150)
-MAROON = (172,127,1)
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-OLIVE = (128,128,0)
-CYAN = (0,255,255)
-PINK = (255,192,203)
-MAGENTA = (255,0,255)
-TAN = (210,180,140)
-TEAL = (0,128,128)
-
-pygame.init()
-pantalla = pygame.display.set_mode(TAMANY)
-pygame.display.set_caption('Color de fons')
-while True: # main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    pantalla.fill(YELLOW)
-    pygame.draw.circle(pantalla, WHITE, (450, 150), 100, )
-    pygame.draw.circle(pantalla, BLACK, (450, 150), 100, 10)
-    pygame.draw.circle(pantalla, BLACK, (300, 150), 100, 10)
-    pygame.draw.rect(pantalla, YELLOW, (340, 170, 100, 80))
-    pygame.draw.rect(pantalla, YELLOW, (340, 170, 100, 80))
-    pygame.draw.rect(pantalla, BLACK, (340, 170, 100, 80),10)
-    pygame.draw.rect(pantalla, YELLOW, (400, 170, 100, 80))
-    pygame.draw.rect(pantalla, BLACK, (400, 170, 100, 80),10)
-    pygame.draw.circle(pantalla, WHITE, (300, 150), 100, )
-    pygame.draw.circle(pantalla, BLACK, (300, 150), 100, 10)
-    pygame.draw.circle(pantalla, BLACK, (530, 150), 10, )
-    pygame.draw.circle(pantalla, BLACK, (380, 150), 10, )
+    if pantalla_actual == 2:
+        show_credits()
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                pantalla_actual = 1
 
 
-    pygame.draw.rect(pantalla, YELLOW, (395, 180, 60, 60))
-    pygame.draw.rect(pantalla, WHITE, (490, 180, 10, 40))
-    pygame.draw.circle(pantalla, BLACK, (460,210 ), 40,10 )
-    pygame.draw.rect(pantalla, YELLOW, (395, 180, 60, 60))
-    pygame.draw.rect(pantalla, YELLOW, (339, 240, 80, 80))
-    pygame.draw.rect(pantalla, WHITE, (485, 170, 30, 5))
-    pygame.draw.rect(pantalla, WHITE, (485, 175, 30, 5))
-    pygame.draw.rect(pantalla, BLACK, (400, 170, 30, 9))
-    pygame.draw.rect(pantalla, YELLOW, (480, 240, 20, 30))
-    pygame.draw.circle(pantalla, MAROON, (400,380), 150)
-    pygame.draw.circle(pantalla, BLACK, (450,350),(50))
+    if pantalla_actual == 1:
+        if event.type == KEYDOWN:
+            if event.key == K_3:
+                pygame.quit()
+            if event.key == K_1:
+                pantalla_actual = 3
+            if event.key == K_2:
+                pantalla_actual = 2
 
-    pygame.display.update()
+
+
+    if pantalla_actual == 4:
+        for i in bales_jugador1:
+            bales_jugador1.remove(i)
+        for i in bales_jugador2:
+            bales_jugador2.remove(i)
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                vides_jugador1 = 3
+                vides_jugador2 = 3
+                pantalla_actual = 1
+
+
+    if pantalla_actual == 3:
+        # controlar trets de les naus
+        if event.type == KEYDOWN:
+            #jugador 1
+            if event.key == K_w and current_time - temps_ultima_bala_jugador1 >= temps_entre_bales:
+                bales_jugador1.append(pygame.Rect(player_rect.centerx - 2, player_rect.top, 4, 10))
+                temps_ultima_bala_jugador1 = current_time
+            # jugador 2
+            if event.key == K_UP and current_time - temps_ultima_bala_jugador2 >= temps_entre_bales:
+                bales_jugador2.append(pygame.Rect(player_rect2.centerx - 2, player_rect2.bottom -10, 4, 10))
+                temps_ultima_bala_jugador2 = current_time
+
+if pantalla_actual == 1:
+    show_menu()
+
+if pantalla_actual == 2:
+    show_credits()
+
+
+
+if pantalla_actual == 4:
+    imprimir_pantalla_fons("assets/game_over.png")
+    text = "Player " + str(guanyador) + " Wins!"
+    font = pygame.font.SysFont(None, 64)
+    img = font.render(text, True, GREEN)
+    pantalla.blit(img, (250,0))
+
+
+if pantalla_actual == 1:
+    show_menu()
+
+if pantalla_actual == 3:
+    # Moviment del jugador 1
+    keys = pygame.key.get_pressed()
+    if keys[K_a]:
+        player_rect.x -= velocitat_nau
+    if keys[K_d]:
+        player_rect.x += velocitat_nau
+    # Moviment del jugador 2
+    if keys[K_LEFT]:
+        player_rect2.x -= velocitat_nau2
+    if keys[K_RIGHT]:
+        player_rect2.x += velocitat_nau2
+
+
+
+    # Mantenir al jugador dins de la pantalla:
+    player_rect.clamp_ip(pantalla.get_rect())
+    player_rect2.clamp_ip(pantalla.get_rect())
+
+    #dibuixar el fons:
+    imprimir_pantalla_fons(BACKGROUND_IMAGE)
+
+    #Actualitzar i dibuixar les bales del jugador 1:
+    for bala in bales_jugador1: # bucle que recorre totes les bales
+        bala.y -= velocitat_bales # mou la bala
+        if bala.bottom < 0 or bala.top > ALTURA: # comprova que no ha sortit de la pantalla
+            bales_jugador1.remove(bala) # si ha sortit elimina la bala
+        else:
+            pantalla.blit(bala_imatge, bala) # si no ha sortit la dibuixa
+        # Detectar col·lisions jugador 2:
+        if player_rect2.colliderect(bala):  # si una bala toca al jugador1 (el seu rectangle)
+            print("BOOM 1!")
+            bales_jugador1.remove(bala)  # eliminem la bala
+            vides_jugador2 = vides_jugador2 -1
+            print("vides jugador 2:", vides_jugador2)
+            # mostrem una explosió
+            # eliminem el jugador 1 (un temps)
+            # anotem punts al jugador 1
+
+    # Actualitzar i dibuixar les bales del jugador 2:
+    for bala in bales_jugador2:
+        bala.y += velocitat_bales
+        if bala.bottom < 0 or bala.top > ALTURA:
+            bales_jugador2.remove(bala)
+        else:
+            pantalla.blit(bala_imatge, bala)
+        # Detectar col·lisions jugador 1:
+        if player_rect.colliderect(bala):  # si una bala toca al jugador1 (el seu rectangle)
+            print("BOOM 2!")
+            bales_jugador2.remove(bala)  # eliminem la bala
+            vides_jugador1 = vides_jugador1 - 1
+            print("vides jugador 1:",vides_jugador1)
+            # mostrem una explosió
+            # eliminem el jugador 1 (un temps)
+            # anotem punts al jugador 1
+
+    #dibuixar els jugadors:
+    pantalla.blit(player_image, player_rect)
+    pantalla.blit(player_image2, player_rect2)
+
+    #dibuixar vides
+    if vides_jugador1 >= 3:
+        pantalla.blit(vides_jugador1_image,(620,550))
+    if vides_jugador1 >= 2:
+        pantalla.blit(vides_jugador1_image, (680, 550))
+    if vides_jugador1 >= 1:
+        pantalla.blit(vides_jugador1_image, (740, 550))
+
+    if vides_jugador2 >= 1:
+        pantalla.blit(vides_jugador2_image,(0,0))
+    if vides_jugador2 >= 2:
+        pantalla.blit(vides_jugador2_image, (60, 0))
+    if vides_jugador2 >= 3:
+        pantalla.blit(vides_jugador2_image, (120, 0))
+
+
+
+    # comprobem si ha perdut
+    if vides_jugador1 <=0 or vides_jugador2<= 0:
+        guanyador = 1
+        if vides_jugador1 <= 0:
+            guanyador = 2
+
+        pantalla_actual = 4
+
+pygame.display.update()
+clock.tick(fps)
